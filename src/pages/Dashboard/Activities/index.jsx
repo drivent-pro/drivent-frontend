3ba4e1134a7b10c8168178faa996d9f709b6fcb7
Caio Lucas <clpmb2000@gmail.com>
2023-10-30 11:18:51 -0300

    feat: vizualização dos dias das atividades

--- src/pages/Dashboard/Activities/index.jsx
+++ src/pages/Dashboard/Activities/index.jsx
@@ -2,11 +2,24 @@ import { styled } from "styled-components";
 import UserContext from "../../../contexts/UserContext";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
+import dayjs from "dayjs";
+import 'dayjs/locale/pt-br';
 
 export default function Activities() {
   const { userData } = useContext(UserContext);
   const [infoByUser, setInfoByUser] = useState(null);
+  const [infoByActivities, setInfoByActivities] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  function getActivities() {
+    axios.get(`${import.meta.env.VITE_API_URL}/activities`)
+      .then(res => {
+        console.log(res.data);
+        setInfoByActivities(res.data);
+      }).catch(err => {
+        console.error(err);
+      })
+  }
 
   function getInfo() {
     axios
@@ -25,11 +38,13 @@ export default function Activities() {
 
   useEffect(() => {
     getInfo();
+    getActivities();
   }, []);
 
   if (loading) {
     return <p>Carregando...</p>;
   }
+
   if (!infoByUser.infoByEnrollment) {
     return (
       <>
@@ -46,18 +61,50 @@ export default function Activities() {
       <>
         <Title>Escolha de hotel e quarto</Title>
         <SubtitleForNoHotel>
-        Sua modalidade de ingresso não necessita escolher<br /> atividade. Você terá acesso a todas as atividades.
+          Sua modalidade de ingresso não necessita escolher<br /> atividade. Você terá acesso a todas as atividades.
         </SubtitleForNoHotel>
       </>
     )
   }
+
+  if (infoByUser.infoByTicket.TicketType.isRemote === true) {
+    return (
+      <>
+        <Title>Escolha de atividades</Title>
+        <SubtitleForModalityOnline>
+          Sua modalidade de ingresso é online.<br />Não é necessário escolher atividades.
+        </SubtitleForModalityOnline>
+      </>
+    )
+  }
+
+  const dayAbbreviations = {
+    "dom": "domingo",
+    "seg": "segunda",
+    "ter": "terça",
+    "qua": "quarta",
+    "qui": "quinta",
+    "sex": "sexta",
+    "sáb": "sábado"
+  };
 
   return (
     <>
       <Title>Escolha de atividades</Title>
       <Subtitle>Primeiro, filtre pelo dia do evento: </Subtitle>
       <ConteinerDays>
-        <EventDay>Sexta, 22/11</EventDay>
+        {infoByActivities.map((activity) => {
+          const startsAt = dayjs(activity.startsAt).locale('pt-br');
+          const dayAbbreviation = startsAt.format("ddd");
+          const formattedDate = dayAbbreviations[dayAbbreviation];
+          const fullDate = startsAt.format("DD/MM");
+
+          return (
+            <EventDay key={activity.id}>
+              {formattedDate} - {fullDate}
+            </EventDay>
+          )
+        })}
       </ConteinerDays>
     </>
   );
@@ -91,9 +138,17 @@ const SubtitleForNoHotel = styled.p`
   text-align: center;
 `
 
+const SubtitleForModalityOnline = styled.p`
+  font-size: 20px;
+  color: rgba(142, 142, 142, 1);
+  line-height: 23.44px;
+  margin-top: 255px;
+  text-align: center;
+`
+
 const ConteinerDays = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
 `
 
 const EventDay = styled.div`
@@ -105,4 +160,6 @@ const EventDay = styled.div`
   align-items: center;
   border-radius: 4px;
   box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
+  margin-right: 25px;
+  margin-bottom: 20px;
 `
